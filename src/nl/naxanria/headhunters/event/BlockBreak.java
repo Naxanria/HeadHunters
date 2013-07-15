e461c1d358b85e72507b53e832ddd1daa8e54a25
Kruithne <kruithne@gmail.com>
2013-07-15 20:22:40 +0100

    Statement merge.

--- src/nl/naxanria/headhunters/event/BlockBreak.java
+++ src/nl/naxanria/headhunters/event/BlockBreak.java
@@ -22,11 +22,8 @@ public class BlockBreak implements IBlockBreakEvent
 		RunsafePlayer eventPlayer = event.getPlayer();
 		if (eventPlayer.getWorld().getName().equalsIgnoreCase(playerHandler.getWorldName()))
 			if (areaHandler.isInCombatRegion(eventPlayer.getLocation()))
-				if (core.isEnabled())
+				if (core.isEnabled() || !eventPlayer.hasPermission("headhunters.build"))
 					event.setCancelled(true);
-				else
-					if (!eventPlayer.hasPermission("headhunters.build"))
-						event.setCancelled(true);
 	}
 
 	private final PlayerHandler playerHandler;
